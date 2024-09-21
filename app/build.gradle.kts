92b3e794cfa38501980c9885770a09d56e3a0af4
ismailmesut <ismailmesutmujde@gmail.com>
2024-09-21 20:52:16 +0300

    updated: coding

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,10 @@ android {
     namespace = "com.ismailmesutmujde.kotlinstoragesharedpreferencesentrycounterapp"
     compileSdk = 34
 
+    buildFeatures {
+        viewBinding = true
+    }
+    
     defaultConfig {
         applicationId = "com.ismailmesutmujde.kotlinstoragesharedpreferencesentrycounterapp"
         minSdk = 24
--- app/src/main/java/com/ismailmesutmujde/kotlinstoragesharedpreferencesentrycounterapp/MainActivity.kt
+++ app/src/main/java/com/ismailmesutmujde/kotlinstoragesharedpreferencesentrycounterapp/MainActivity.kt
@@ -1,13 +1,30 @@
 package com.ismailmesutmujde.kotlinstoragesharedpreferencesentrycounterapp
 
+import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.ismailmesutmujde.kotlinstoragesharedpreferencesentrycounterapp.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var bindingMain : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        bindingMain = ActivityMainBinding.inflate(layoutInflater)
+        val view = bindingMain.root
+        setContentView(view)
+
+        val sharedPreferences = getSharedPreferences("EntryCounter", Context.MODE_PRIVATE)
+
+        var counter = sharedPreferences.getInt("counter", 0)
+
+        val editor = sharedPreferences.edit()
+        editor.putInt("counter", ++counter)
+        editor.commit()
+
+        bindingMain.textViewCounter.text = "Entry Counter : $counter"
 
     }
 }
